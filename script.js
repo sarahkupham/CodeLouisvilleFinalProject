009cfc826f8702571b3cb586248ef97e81d09fde
unknown <you@example.com>
2021-07-27 17:59:25 -0400

    added email validation and response window for invalid email addresses. also updated the readme file and added the mailing list signup to all pages.

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-function validateEmail(email) 
-    {
-        var re = /\S+@\S+\.\S+/;
-        return re.test(email);
-    }
-    
-console.log(validateEmail('anystring@anystring.anystring'));
+function sConsole() {
+    var data = document.getElementById("email");
+    var dataValue = data.value;
+    console.log(data.value);
+    var email = document.getElementById('email');
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if (!filter.test(email.value)) {
+      
+    alert(document.getElementById('email').value + ' is not valid. Please enter a valid email address and resubmit.');
+    email.focus;
+    return false;
+ }
+
+
+
+ sConsole();
+
+var submitButton = document.getElementById("submit");
+submitButton.addEventListener("click",(e)=>{e.preventDefault(); sConsole();}) 
+}
